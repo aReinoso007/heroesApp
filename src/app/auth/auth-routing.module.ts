e550b23ee83cd7279d99c7b72ef8e0bc0f185253
Alex Reinoso <ralexjessiel@gmail.com>
2023-12-06 10:33:19 -0500

    routes

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutPageComponent } from './pages/layout/layout-page.component';
 
 const routes: Routes = [
-
+    { path:'',
+    component: LayoutPageComponent}
 ];
 
 @NgModule({
